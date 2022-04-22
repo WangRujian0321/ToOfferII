f61f42f5dfa2788f2384e526904125d913a5ef70
wrj <846741270@qq.com>
2022-04-22 10:35:06 +0800

    20220422——1

--- dp/416.py
+++ dp/416.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition(self, nums) -> bool:
         nums_sum = sum(nums)
         if nums_sum % 2:
             return False
@@ -10,3 +10,6 @@ class Solution:
             for i in range(target, num - 1, -1):
                 dp[i] = dp[i] or dp[i - num]
         return dp[-1]
+
+sol = Solution()
+print(sol.canPartition([1,5,11,5]))
